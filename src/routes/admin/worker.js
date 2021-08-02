b75ecac2ee74f652068e0e1079aed87cac3b3f88
subin <subin.thomas@threestops.com>
2021-08-02 19:08:41 +0530

    validator fix

--- src/routes/admin/worker.js
+++ src/routes/admin/worker.js
@@ -2,38 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const workerController = require("../../controllers/admin/WorkerAdminController");
-const {body} = require("express-validator");
-const { Users,Workers } = require("../../models");
 
-const workerCreateValidator = [
-    body("email")
-        .isEmail()
-        .withMessage("Please enter a valid email.")
-        .custom((value, { req }) => {
-            return Users.findOne({ where: { email: value } }).then((userDoc) => {
-                if (userDoc) {
-                    return Promise.reject("E-Mail address already exists in Users!");
-                }
-                return Workers.findOne({ where: { email: value } })
-            }).then((userDoc) => {
-                if (userDoc) {
-                    return Promise.reject("E-Mail address already exists in Workers!");
-                }
-            });
-        })
-        .normalizeEmail(),
-
-    body("professionId").trim().not().isEmpty().withMessage("Profession Shouldn't be Empty"),
-];
+const {
+    validateReq,
+    workerCreateValidator,
+  } = require("../../validators/worker-validator");
 
 
 router.get('/', workerController.getAllWorkers);
 
 router.get('/profession/:professionId', workerController.getAllWorkersByProfessionId);
 
-router.post('/', workerCreateValidator,workerController.createWorkers);
+router.post('/', [workerCreateValidator], workerController.createWorkers);
 
-router.get('/:id', workerController.getWorkersById)
+router.get('/:id', [validateReq], workerController.getWorkersById)
 
 router.put('/:id', workerController.updateWorkersbyId)
 
